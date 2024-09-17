9a9fdbb86e0f31451db5dc033f2145475c2a185e
MeerMuneeb <jimmykuddon@gmail.com>
2024-09-17 23:59:52 +0500

    80 Percent FE Complete

---
+++ src/utils/demo/tickets.js
@@ -0,0 +1,401 @@
+export default [{
+    "id": 1,
+    "role": "User",
+    "senderID": "01J7R1VAQYB36X354BM4YC346H",
+    "subject": "Quisque id justo sit amet sapien dignissim vestibulum.",
+    "message": "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "3/2/2024",
+    "time": "10:37 AM"
+  }, {
+    "id": 2,
+    "role": "User",
+    "senderID": "01J7R1VAN9YCNCM02ABY7YB4JD",
+    "subject": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+    "message": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "1/2/2024",
+    "time": "3:11 AM"
+  }, {
+    "id": 3,
+    "role": "User",
+    "senderID": "01J7R1VAVZZ5VZ12JS6B8V4D4K",
+    "subject": "Cras in purus eu magna vulputate luctus.",
+    "message": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "5/15/2024",
+    "time": "1:11 AM"
+  }, {
+    "id": 4,
+    "role": "User",
+    "senderID": "01J7R1VAWDH0EHZR0JR0T4ETEC",
+    "subject": "Pellentesque viverra pede ac diam.",
+    "message": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "8/12/2024",
+    "time": "8:29 PM"
+  }, {
+    "id": 5,
+    "role": "User",
+    "senderID": "01J7R1VAR5HTE6HZ29WAVYPSYF",
+    "subject": "Nunc nisl.",
+    "message": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "1/15/2024",
+    "time": "7:39 PM"
+  }, {
+    "id": 6,
+    "role": "User",
+    "senderID": "01J7R1VAQJM3N39BWJVMV1CJ2G",
+    "subject": "Etiam pretium iaculis justo.",
+    "message": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "1/14/2024",
+    "time": "7:27 PM"
+  }, {
+    "id": 7,
+    "role": "User",
+    "senderID": "01J7R1VAW6PBZY3F7PGS206S1W",
+    "subject": "Nulla tempus.",
+    "message": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "1/25/2024",
+    "time": "5:34 AM"
+  }, {
+    "id": 8,
+    "role": "User",
+    "senderID": "01J7R1VAQW4JS8XXGB0CDYPDYV",
+    "subject": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.",
+    "message": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "7/26/2024",
+    "time": "10:36 PM"
+  }, {
+    "id": 9,
+    "role": "User",
+    "senderID": "01J7R1VANR3CKVGRAQXM86TVHQ",
+    "subject": "Aenean lectus.",
+    "message": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "5/12/2024",
+    "time": "6:56 PM"
+  }, {
+    "id": 10,
+    "role": "User",
+    "senderID": "01J7R1VARRA1NEQN5ZXXN85QM0",
+    "subject": "Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+    "message": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "6/16/2024",
+    "time": "5:18 PM"
+  }, {
+    "id": 11,
+    "role": "User",
+    "senderID": "01J7R1VAS1JB3M0MD8W3K7R6Q9",
+    "subject": "Integer ac leo.",
+    "message": "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "6/15/2024",
+    "time": "10:39 AM"
+  }, {
+    "id": 12,
+    "role": "User",
+    "senderID": "01J7R1VANR3CKVGRAQXM86TVHQ",
+    "subject": "In quis justo.",
+    "message": "Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "4/1/2024",
+    "time": "5:48 PM"
+  }, {
+    "id": 13,
+    "role": "User",
+    "senderID": "01J7R1VANR3CKVGRAQXM86TVHQ",
+    "subject": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.",
+    "message": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "10/16/2023",
+    "time": "6:11 PM"
+  }, {
+    "id": 14,
+    "role": "User",
+    "senderID": "01J7R1VARJ9Q3RXR7GSX03VA4C",
+    "subject": "Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
+    "message": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "11/29/2023",
+    "time": "12:33 AM"
+  }, {
+    "id": 15,
+    "role": "User",
+    "senderID": "01J7R1VAPHVWDWQ95XES7BYDZB",
+    "subject": "Curabitur at ipsum ac tellus semper interdum.",
+    "message": "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "12/16/2023",
+    "time": "8:45 PM"
+  }, {
+    "id": 16,
+    "role": "User",
+    "senderID": "01J7R1VAQ24V4S3F806C6A69Y9",
+    "subject": "Nulla nisl.",
+    "message": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "4/4/2024",
+    "time": "4:47 PM"
+  }, {
+    "id": 17,
+    "role": "User",
+    "senderID": "01J7R1VAPX7M2TKKC6680C55Z1",
+    "subject": "Nullam porttitor lacus at turpis.",
+    "message": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "1/8/2024",
+    "time": "9:13 PM"
+  }, {
+    "id": 18,
+    "role": "User",
+    "senderID": "01J7R1VAQC31D85QW5TQT0RQRJ",
+    "subject": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+    "message": "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "10/2/2023",
+    "time": "9:37 PM"
+  }, {
+    "id": 19,
+    "role": "User",
+    "senderID": "01J7R1VAQZCRMSY72DB50F58CZ",
+    "subject": "Aliquam sit amet diam in magna bibendum imperdiet.",
+    "message": "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "5/9/2024",
+    "time": "5:32 PM"
+  }, {
+    "id": 20,
+    "role": "User",
+    "senderID": "01J7R1VAQ24V4S3F806C6A69Y9",
+    "subject": "Curabitur gravida nisi at nibh.",
+    "message": "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+    "attachments": [
+      {
+        "url": "https://picsum.photos/seed/1234/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/5678/300/300"
+      },
+      {
+        "url": "https://picsum.photos/seed/9101/300/300"
+      }
+    ],
+    "isRead": false,
+    "date": "4/30/2024",
+    "time": "8:08 PM"
+  }]
